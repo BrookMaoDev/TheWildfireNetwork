a855b496d479c7531edc26c55ef5588f638a7911
Brook Mao <bmao2088@gmail.com>
2023-08-26 16:52:41 -0400

    News feature

---
+++ home/util.py
@@ -0,0 +1,21 @@
+import feedparser
+
+
+def getFeed():
+    feedUrl = "https://www.cbc.ca/webfeed/rss/rss-canada"
+    feed = feedparser.parse(feedUrl)
+    posts = feed.entries
+    wildfireNews = []
+
+    for post in posts:
+        if "wildfire" in post.title or "Wildfire" in post.title:
+            temp = dict()
+            temp["Title"] = post.title
+            temp["Time Published"] = post.published
+            temp["Link"] = post.link
+            wildfireNews.append(temp)
+
+    return wildfireNews
+
+
+print(getFeed())
