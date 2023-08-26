2ef56f6fa3307100c25b3ac1443c4b2b430961e6
Brook Mao <bmao2088@gmail.com>
2023-08-26 18:02:32 -0400

    moved my news function from util to news.py

---
+++ home/news.py
@@ -0,0 +1,22 @@
+import feedparser
+
+
+def getFeed():
+    feedUrl = "https://www.cbc.ca/webfeed/rss/rss-canada"
+    feed = feedparser.parse(feedUrl)
+    posts = feed.entries
+    wildfireNews = []
+
+    for post in posts:
+        if "wildfire" in post.title or "Wildfire" in post.title:
+            temp = dict()
+            temp["Title"] = post.title
+            temp["Time Published"] = post.published
+            temp["Link"] = post.link
+            temp["Summary"] = post.summary
+            wildfireNews.append(temp)
+
+    return wildfireNews
+
+
+getFeed()
--- home/util.py
+++ home/util.py
@@ -38,22 +38,3 @@ class Location:
             return response.json()
         else:
             return False
-import feedparser
-
-
-def getFeed():
-    feedUrl = "https://www.cbc.ca/webfeed/rss/rss-canada"
-    feed = feedparser.parse(feedUrl)
-    posts = feed.entries
-    wildfireNews = []
-
-    for post in posts:
-        if "wildfire" in post.title or "Wildfire" in post.title:
-            temp = dict()
-            temp["Title"] = post.title
-            temp["Time Published"] = post.published
-            temp["Link"] = post.link
-            temp["Summary"] = post.summary
-            wildfireNews.append(temp)
-
-    return wildfireNews
