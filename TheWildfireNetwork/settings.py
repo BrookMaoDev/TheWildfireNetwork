87e8805df6548b0d1188bfe39f1cecb9f5694cf8
Owen Wang <owenwang0099@gmail.com>
2023-08-26 20:41:17 -0400

    wildfire prediction

--- TheWildfireNetwork/settings.py
+++ TheWildfireNetwork/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "geoip2",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+GEOIP_PATH = os.path.join("geoip")
--- home/views.py
+++ home/views.py
@@ -1,10 +1,48 @@
 from django.shortcuts import render
+from django.contrib.gis.geoip2 import GeoIP2
+from django.http import HttpResponse, HttpResponseRedirect
+from django import forms
+from django.urls import reverse
 from . import util
 
 
-# Create your views here.
 def index(request):
-    location = util.Location("Toronto")  # pass city or postal code as param
-    return render(request, "home/index.html", {
-        'response': location.getForecast()
-    })
+    if not request.session["postal"] or not request.session["city"]:
+        user_ip_address = request.META.get("HTTP_X_FORWARDED_FOR")
+        if user_ip_address:
+            ip = user_ip_address.split(",")[0]
+        else:
+            ip = request.META.get("REMOTE_ADDR")
+
+        try:
+            g = GeoIP2()
+            location = g.city("192.206.151.131")
+            request.session["city"] = location["city"]
+            request.session["postal"] = location["postal_code"]
+        except:
+            request.session["city"] = None
+            request.session["postal"] = None
+
+    location = util.Location(request.session["city"], request.session["postal"])
+    return render(request, "home/index.html", {"response": location.getForecast()})
+
+
+def changeLocation(request):
+    return HttpResponse("Change Location")
+
+
+def predict(request):
+    if request.method == "POST":
+        # verify if city and postal are valid
+        form = predictForm(request.POST)
+        if form.is_valid():
+            request.session["postal"] = form.cleaned_data["postal"]
+            request.session["city"] = form.cleaned_data["city"]
+            return HttpResponseRedirect(reverse("index"))
+
+    return render(request, "home/predict.html", {"predictForm": predictForm()})
+
+
+class predictForm(forms.Form):
+    postal = forms.CharField(label="Postal Code")
+    city = forms.CharField(label="City")
