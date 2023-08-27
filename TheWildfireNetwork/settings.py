54370fb7c52b57011ee9293c01fa43bea7d8259a
Owen Wang <owenwang0099@gmail.com>
2023-08-27 04:27:14 -0400

    vercel integration

--- TheWildfireNetwork/settings.py
+++ TheWildfireNetwork/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = "django-insecure-2crp*sf_=%&u+m@ey$v!d*4=968(_gw3=(1__e5#55+ztep@hl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
