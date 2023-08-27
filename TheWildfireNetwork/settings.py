ff9ac5eed5cb278ba338b3e7e1e61a20951d4f10
Owen Wang <owenwang0099@gmail.com>
2023-08-27 11:39:09 -0400

    vercel deployment bugfix

--- TheWildfireNetwork/settings.py
+++ TheWildfireNetwork/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = "TheWildfireNetwork.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
+        "ENGINE": "django.db.backends.pysqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
